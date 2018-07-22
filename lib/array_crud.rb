56637d9958fae3254cb390b339920c424e14d49a
Madeline Celis <madelinebcelis@gmail.com>
2018-07-22 23:34:12 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,12 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = [1,2,3,4];
+  array = [1,2,3,4]
   return array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  return array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
